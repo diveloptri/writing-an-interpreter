b5b7d4f4f03795f621c7f3b213316fb9aceb4586
diveloptri <diveloptri@gmail.com>
2025-07-04 17:33:18 +0200

    feat(string datatype): support string concatenation

--- chimera/internal/evaluator/evaluator_test.go
+++ chimera/internal/evaluator/evaluator_test.go
@@ -242,6 +242,10 @@ func TestErrorHandling(t *testing.T) {
 			"foobar",
 			"identifier not found: foobar",
 		},
+		{
+			`"Hello" - "World"`,
+			"unknown operator: STRING - STRING",
+		},
 	}
 
 	for _, tt := range tests {
@@ -337,6 +341,20 @@ func TestStringLiteral(t *testing.T) {
 	str, ok := evaluated.(*object.String)
 	if !ok {
 		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Errorf("object is not String. got=%T (%+v)", evaluated, evaluated)
 	}
 
 	if str.Value != "Hello World!" {
