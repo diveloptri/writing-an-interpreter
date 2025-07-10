2694bba2f52f23bae7ef204994a7e33f1e393e6d
diveloptri <diveloptri@gmail.com>
2025-07-10 18:46:35 +0200

    feat(builtins): add puts function

--- chimera/internal/evaluator/builtins.go
+++ chimera/internal/evaluator/builtins.go
@@ -1,6 +1,9 @@
 package evaluator
 
-import "goInterpreter/chimera/internal/object"
+import (
+	"fmt"
+	"goInterpreter/chimera/internal/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": &object.Builtin{
@@ -18,6 +21,15 @@ var builtins = map[string]*object.Builtin{
 			default:
 				return newError("argument to `len` not supported, got %s", args[0].Type())
 			}
+		},
+	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 	"first": &object.Builtin{
