7a6b8d2d7009d94835e4693331c931b9ac7ace5e
diveloptri <diveloptri@gmail.com>
2025-08-06 19:11:10 +0200

    feat(repl): extend repl to include parser

--- chimera/src/repl/repl.rs
+++ chimera/src/repl/repl.rs
@@ -1,21 +1,25 @@
 use std::io::{self, Write};
+use crate::ast::ast::Node;
 use crate::lexer::lexer::Lexer;
-use crate::token::token::*;
+use crate::parser::parser;
 
 pub const PROMPT: &'static str = ">> ";
+pub const CHIMERA: &'static str = r#"
+(╯°□°）╯︵ ┻━┻ 
+"#;
 
 pub fn start_repl() {
     loop {
-        let mut lexer = Lexer::new(prompt());
+        let lexer = Lexer::new(prompt());
+        let mut parser = parser::Parser::new(lexer);
+        let program = parser.parse_program();
 
-        loop {
-            let token = lexer.next_token();
-            if token.token_type == EOF {
-                break;
-            }
-            println!("{:?}", token);
-            
-        }
+        if parser.errors().len() != 0 {
+            print_parser_error(parser.errors());
+            continue;
+        };
+        
+        println!("{}", program.string());
     }
 }
 
@@ -29,4 +33,13 @@ fn prompt() -> String {
         .expect("Failed to read line");
 
     return user_input.trim().to_string()
+}
+
+fn print_parser_error(errors: Vec<String>) {
+    println!("{}", CHIMERA);
+    println!("Woops! We ran into some errors here!");
+    println!("Parser errors:");
+    errors.iter().for_each(
+        |e| println!("\t{}", e)
+    );
 }
